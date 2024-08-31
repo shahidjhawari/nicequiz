b1519110f7f00923e1b1d52b84065a511cc4b158
Shahid Iqbal <shahidiqbaljhawari@gmail.com>
2024-08-31 16:35:35 +0500

    latest

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+import App from './App';  // Ensure the path to your main App component is correct
+import { name as appName } from './app.json';  // Ensure the app name is imported correctly
 
 AppRegistry.registerComponent(appName, () => App);
